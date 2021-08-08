926b108ec2379a5c64d7e919406e4dbb63f2051f
Kerim Hamadi <88054136+nichtkerim@users.noreply.github.com>
2021-08-08 03:05:25 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const ytdl = require("ytdl-core");
+
+client.config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  client.user.setActivity(client.config.activity, { type: "PLAYING" });
+});
+
+client.on("ready", async () => {
+  const channel = client.channels.cache.get(client.config.channel);
+  if (!channel) return;
+
+  const connection = await channel.join();
+  connection.play(ytdl(client.config.live));
+});
+
+setInterval(async function() {
+  if (!client.voice.connections) {
+    const channel = client.channels.cache.get(client.config.channel);
+    if (!channel) return;
+
+    const connection = await channel.join();
+    connection.play(ytdl(client.config.live));
+  }
+}, 30000);
+
+client.login(client.config.token);
